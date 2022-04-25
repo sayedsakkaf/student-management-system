9de60d277d708e9beee7569def0176942e312eb0
Sayed <sayedsakkaf1@hotmail.com>
2022-04-25 12:13:14 +0100

    Updates to Course entity to make student work

--- src/main/java/com/qa/student_management_system/domain/Course.java
+++ src/main/java/com/qa/student_management_system/domain/Course.java
@@ -4,9 +4,12 @@ import java.util.Objects;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
@@ -29,6 +32,10 @@ public class Course {
 	@NotNull
 	@NotBlank
 	private String courseDescription;
+	
+	@ManyToOne(optional = true, fetch = FetchType.LAZY)
+	@JoinColumn(name = "student_id", referencedColumnName = "id")
+	private Student student;
 	
 	public Course() {
 		super();
